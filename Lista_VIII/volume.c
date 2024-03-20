6aa8793b2a075cc1f68f779d499517f169c631a9
MagnoQueiroz <synxther@protonmail.com>
2024-03-20 17:17:53 -0300

    codigo

---
+++ Lista_VIII/volume.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <locale.h>
+
+float volume(float lag, float comp, float alt);
+
+int main()
+{
+    float lag, comp, alt, ret;
+    printf("digite a largura, comprimento e altura: ");
+    scanf("%f %f %f", &lag, &comp, &alt);
+
+    ret = volume(lag, comp, alt);
+
+    printf("o volume é: %.2f", ret);
+    return 0;
+
+}
+
+float volume(float lag, float comp, float alt) {
+   float resultado = lag * comp* alt;
+
+   return(resultado);
+}
