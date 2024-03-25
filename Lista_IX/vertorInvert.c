c004e0640413a7240c7c3d4a6ac8af7c6402791f
MagnoQueiroz <synxther@protonmail.com>
2024-03-25 16:51:22 -0300

    codigo

---
+++ Lista_IX/vertorInvert.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "portuguese");
+    int v[8];
+    int i;
+    for(i = 0; i< 8; i++) {
+        printf("Digite um numero: ");
+        scanf("%d", &v[i]);
+
+    }
+    for(i= 7; i>=0; i--) {
+        printf("%dº número: %d\n",(i+1), v[i]);
+    }
+
+    return(0);
+}
