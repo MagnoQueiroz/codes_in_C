6a66fa5d6dd4f30cd6a90b2b915fd5c4ffd636d9
MagnoQueiroz <synxther@protonmail.com>
2024-04-04 13:25:19 -0300

    edit style code

--- Lista_X/multMatrix.c
+++ Lista_X/multMatrix.c
@@ -1,30 +1,41 @@
 #include <stdio.h>
 
 int main() {
-    int x, y;
-    int A[x][y], B[x][y];
+    int  i,j,x, y;
 
+
+
+    int z, c = 1;
     printf("Digite a quantidade de linhas: ");
     scanf("%d", &x);
     printf("Digite a quantidade de colunas: ");
     scanf("%d", &y);
 
-    for (int i = 0; i < x; i++) {
-        for (int j = 0; j < y; j++) {
-            printf("Digite o %d de A: ", c++);
-            scanf("%d", &A[i][j]);
+    int B[x][y];
+    int A[x][y];
+
+    for(i=0; i<x; i++) {
+        for(j=0; j<y; j++) {
+            printf("Digite o %dº numero: ", c++);
+            scanf("%d",&A[i][j]);
+
         }
     }
+    printf("Matrix A\n");
+    for(i=0; i<x; i++) {
+        for (j = 0; j <y ; j++) {
+            printf(" %d ", A[i][j]);
 
+        }printf("\n");
+    }
     printf("Matrix B\n");
-    for (int i = 0; i < x; i++) {
-        for (int j = 0; j < y; j++) {
-            B[i][j] = A[i][j] *3;
-
-            printf("| %d ", B[i][j]);
+    for (i=0; i<x; i++) {
+        for (j=0; j <y; j++) {
+            z = A[i][j] * 3;
+            B[i][j] = z;
+            printf(" %d ", B[i][j]);
         }
-        printf("|\n");
+        printf("\n");
     }
-
 }
 
